ca6e4bbd0321c3a47420ea1bfdbb82dda56a245e
xiaoowuu <xiaoowuu@gmail.com>
2019-01-18 17:23:00 +0800

    INDEX

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,19 @@
 import Taro, { Component } from "@tarojs/taro"
-import { View, Image } from "@tarojs/components"
+import { View, Image, ScrollView } from "@tarojs/components"
 import EditItem from "../../components/editItem/editItem"
 import check from "../../image/check.png"
 import "./index.css"
 import AddButton from "../../components/addButton/addButton";
 import AddItem from "../../components/addItem/addItem";
+
+const keys = {
+  znjy: "znjy",
+  jxjy: "jxjy",
+  dbyl: "dbyl",
+  zfdklx: "zfdklx",
+  zfzj: "zfzj",
+  sylr: "sylr",
+}
 
 export default class Index extends Component {
 
@@ -16,9 +25,35 @@ export default class Index extends Component {
     super(props);
     this.items = [];
     this.checkedItems = {};
+    this.tags = [
+      {
+        key: keys.znjy,
+        name: "子女教育",
+      },
+      {
+        key: keys.jxjy,
+        name: "继续教育",
+      },
+      {
+        key: keys.dbyl,
+        name: "大病医疗",
+      },
+      {
+        key: keys.zfdklx,
+        name: "住房贷款利息",
+      },
+      {
+        key: keys.zfzj,
+        name: "住房租金",
+      },
+      {
+        key: keys.sylr,
+        name: "赡养老人",
+      }
+    ];
     this.state = {
       addItem: false
-    }
+    };
   }
 
   componentWillMount() { }
@@ -32,49 +67,58 @@ export default class Index extends Component {
   componentDidHide() { }
 
   render() {
+    let selections;
+    if (this.state.addItem) {
+      selections = this.tags.map(item => {
+        if (selections !== undefined) {
+          return [
+            <AddItem tag={item.key} checked={this.checkedItems[item.key] !== undefined} title={item.name} onCheckChanged={this.setItemChecked.bind(this)} />,
+            <View className="border" />
+          ]
+        }
+        return <AddItem tag={item.key} checked={this.checkedItems[item.key] !== undefined} title={item.name} onCheckChanged={this.setItemChecked.bind(this)} />
+      });
+    }
     let addItems;
     if (this.items.length > 0) {
-      console.log(this.items)
       addItems = this.items.map(item => {
-        if (addItems) {
+        if (addItems !== undefined) {
           return [
             <EditItem title={item.title} placeholder={"••••••"} inputType={"number"} />,
             <View className="border" />
           ]
         }
         return <EditItem title={item.title} placeholder={"••••••"} inputType={"number"} />;
-      })
+      });
     }
     return (
       this.state.addItem ?
         <View className="float-container" >
           <View className="section">
-            <AddItem tag={"fangdai"} checked={this.checkedItems.fangdai} title={"房贷"} onCheckChanged={this.setItemChecked.bind(this)} />
-            <View className="border" />
-            <AddItem tag={"zinv"} checked={this.checkedItems.zinv} title={"子女"} onCheckChanged={this.setItemChecked.bind(this)} />
-            <View className="border" />
-            <AddItem tag={"fumu"} checked={this.checkedItems.fumu} title={"父母"} onCheckChanged={this.setItemChecked.bind(this)} />
+            {selections}
           </View>
           <View className="section" onClick={this.finishAddItem.bind(this)}>
             <AddButton title={"确认"} />
           </View>
         </View> :
-        <View className="container">
-          <View className="content">
-            <View className="section">
-              <EditItem title={"税前月薪"} placeholder={"••••••"} inputType={"number"} />
-              <View className="border" />
-              <EditItem title={"社保个人缴纳"} placeholder={"••••••"} inputType={"number"} />
-              <View className="border" />
-              <EditItem title={"公积金个人缴纳"} placeholder={"••••••"} inputType={"number"} />
+        <View className="container background">
+          <ScrollView className="scrollView">
+            <View className="content">
+              <View className="section">
+                <EditItem ref={"salary"} title={"税前月薪"} placeholder={"••••••"} inputType={"number"} />
+                <View className="border" />
+                <EditItem title={"社保个人缴纳"} placeholder={"••••••"} inputType={"number"} />
+                <View className="border" />
+                <EditItem title={"公积金个人缴纳"} placeholder={"••••••"} inputType={"number"} />
+              </View>
+              <View className="section" >
+                {addItems}
+              </View>
+              <View className="section" onClick={this.addItem.bind(this)}>
+                <AddButton title={"添加专项附加扣除"} />
+              </View>
             </View>
-            <View className="section" >
-              {addItems}
-            </View>
-            <View className="section" onClick={this.addItem.bind(this)}>
-              <AddButton title={"添加专项附加扣除"} />
-            </View>
-          </View>
+          </ScrollView>
           <View className="float-button" onClick={this.commit}>
             <Image className="float-button-icon" src={check} />
           </View>
@@ -99,7 +143,11 @@ export default class Index extends Component {
   }
 
   setItemChecked(key, title, isCheck) {
-    this.checkedItems[key] = { key: key, title: title, isCheck: isCheck };
+    if (isCheck) {
+      this.checkedItems[key] = { key: key, title: title, isCheck: isCheck };
+    } else if (this.checkedItems[key]) {
+      delete this.checkedItems[key]
+    }
   }
 
 }
